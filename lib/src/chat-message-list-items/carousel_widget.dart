522478290fe03227192313bdfd02c8ddb8d930b0
Tevfik Baş (ETIYA) <tevfik.bas@etiya.com>
2024-03-20 12:05:03 +0300

    fix: image viewer path updated

--- lib/src/chat-message-list-items/carousel_widget.dart
+++ lib/src/chat-message-list-items/carousel_widget.dart
@@ -1,7 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:styled_widget/styled_widget.dart';
-import 'package:swifty_chat/src/chat-message-list-items/image_viewer.dart';
+import 'package:swifty_chat/src/utils/image_viewer.dart';
 import 'package:swifty_chat/src/chat.dart';
 import 'package:swifty_chat/src/extensions/theme_context.dart';
 import 'package:swifty_chat/src/protocols/has_avatar.dart';
